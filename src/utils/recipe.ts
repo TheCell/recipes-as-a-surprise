db4256a348fe75e91538f3cd92f3453adfdd8df2
Simon Hischier <simon_hischier@hotmail.com>
2024-04-05 13:37:52 +0200

    added ohter recipes

--- src/utils/recipe.ts
+++ src/utils/recipe.ts
@@ -1,8 +1,40 @@
-import { getCollection } from 'astro:content'
+import { getCollection, type ImageFunction } from 'astro:content'
 
 export const getRecipes = async (max?: number) => {
 	return (await getCollection('recipe'))
 		.filter((recipe) => !recipe.data.draft)
 		.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
 		.slice(0, max)
+}
+
+interface RecipeTeaser {
+	title: string;
+	pubDate: Date;
+	slug: string;
+	image: {
+		src: string;
+		width: number;
+		height: number;
+		format: "png" | "jpg" | "jpeg" | "tiff" | "webp" | "gif" | "svg" | "avif";
+	}; // I will have to clean this up later
+}
+
+export const getLatestRecipes = async (max?: number, excludeId?: string ) => {
+	const teasers: Array<RecipeTeaser> = [];
+	(await getCollection('recipe'))
+		.filter((recipe) => !recipe.data.draft && recipe.id !== excludeId)
+		.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
+		.slice(0, max)
+		.forEach((recipe) => {
+			const recipeTeaser: RecipeTeaser = {
+				title: recipe.data.title,
+				pubDate: recipe.data.pubDate,
+				slug: recipe.slug,
+				image: recipe.data.image
+			}
+			teasers.push(recipeTeaser);
+		});
+	
+
+	return teasers;
 }
